55e0da2a733f9a6d4fccd793ce6cd941c5767514
Shea Newton <shnewto@gmail.com>
2017-05-16 20:56:51 -0700

    simple twitter poem responder

---
+++ bot-request.js
@@ -0,0 +1,22 @@
+console.log("The bot is starting")
+
+var Twit = require('twit');
+
+var config = require('./config');
+var T = new Twit(config);
+
+var params = {
+    q: "boaat",
+    counr: 2
+}
+
+T.get('search/tweets', params, gotData);
+
+function gotData(err, data, response){
+    var tweets = data.statuses;
+    for (var i = 0; i < tweets.length; i++){
+        console.log("----");
+        console.log(tweets[i].text);
+        console.log("----");
+    }
+}
---
+++ config.js
@@ -0,0 +1,5 @@
+
+module.exports = {
+  // fill in the blanks...
+}
+
